b5af348067decc6b50b30902fb0ca666301e1f2c
Palak Arora <palakarora2110@gmail.com>
2019-05-28 17:18:59 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hey world', function(done) {
+ it('respond with Hello everyone @OTS.', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('hey world', done);
+ request(app).get('/').expect('Hello everyone @OTS.', done);
  });
 });
