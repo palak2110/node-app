1effad36aa73c888751d2aaa4f64f72101451570
Palak Arora <palakarora2110@gmail.com>
2019-05-29 13:36:01 +0530

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../index.js');
 describe('GET /', function() {
  it('respond with Greeting message', function(done) {
  //navigate to root and check the the response is "hello world"
- request(app).get('/').expect('Lets confirm with @OTS. Again', done);
+ request(app).get('/').expect('hello', done);
  });
 });
